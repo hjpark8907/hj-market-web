0a1bc7762716b0966c3b8a19d37353f10a0ebb27
hjpark8907 <msyamun@gmail.com>
2021-04-23 05:17:39 +0900

    antd basic application

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 import './App.css';
 import MainPageComponent from "./main";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Link, useHistory } from "react-router-dom";
 import UploadPage from "./upload";
 import ProductPage from "./product";
-
+import { Button } from "antd";
+import { DownloadOutlined } from "@ant-design/icons";
 function App() {
   //return <MainPageComponent />;
+  const history = useHistory();
   return (
     <div>
-      <Switch>
-        <Route exact={true} path={"/"}>
-          <MainPageComponent />
-        </Route>
-        <Route exact={true} path="/products/:id">
-          <ProductPage />
-        </Route>
-        <Route exact={true} path="/upload">
-          <UploadPage />
-        </Route>
-      </Switch>
-    </div>
+      <div id="header">
+        <div id="header-area">
+          <Link to="/">
+            <img src="images/icons/logo2.png" />
+          </Link>
+          <Button size="large"
+            onClick={function () {
+              history.push('/upload');
+            }}
+            icon={<DownloadOutlined />}>Upload items</Button>
+        </div>
+      </div>
+
+      <div id="body">
+        <Switch>
+          <Route exact={true} path={"/"}>
+            <MainPageComponent />
+          </Route>
+          <Route exact={true} path="/products/:id">
+            <ProductPage />
+          </Route>
+          <Route exact={true} path="/upload">
+            <UploadPage />
+          </Route>
+        </Switch>
+      </div>
+      <div id="footer"></div>
+    </div >
   );
 }
 
--- src/product/index.js
+++ src/product/index.js
@@ -1,9 +1,41 @@
 import { useParams } from "react-router-dom";
-
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./index.css"
 function ProductPage() {
     const { id } = useParams();
-    return <h1>useParams {id} product</h1>;
+    const [product, setProduct] = useState(null);
+    useEffect(function () {
+        axios.get(`https://6683a955-ec5e-4bc2-9a98-06f1f79c4b5c.mock.pstmn.io/products/${id}`)
+            .then(
+                function (result) {
+                    setProduct(result.data);
+                })
+            .catch(function (error) {
+                console.error(error);
+            });
+    }, [])
 
+    if (product === null) {
+        return <h1>product information gathering..</h1>
+    }
+    return (
+        <div>
+            <div id="image-box">
+                <img src={"/" + product.imageUrl} />
+            </div>
+            <div id="profile-box">
+                <img src="/images/icons/avatar.png" />
+                <span>{product.seller}</span>
+            </div>
+            <div id="contents-box">
+                <div id="name">{product.name}</div>
+                <div id="price">{product.price}won</div>
+                <div id="createdAt">2021.04.23</div>
+                <div id="description">{product.description}</div>
+            </div>
+        </div>
+    )
 }
 
 export default ProductPage;
